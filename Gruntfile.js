69736071e75b9a5ff9d5c73f4f5575d01aea64d7
jdn <james.neushul@gmail.com>
2015-07-05 13:23:33 -0700

    Installed grunt

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy:{
+            build:{
+                cwd:'app',
+                src:['**'],
+                dest:'build',
+                expand:true
+            }
+        },
+        clean:{
+            build:{
+                src:'build'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask(
+        'build',
+        'Compiles all the assets and copies the files to the build directory.',
+       [ 'clean', 'copy' ]
+    );
+};
