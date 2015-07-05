d6dc9b535caf6635bfe572a231b3765d1e0153f3
jdn <james.neushul@gmail.com>
2015-07-05 13:47:28 -0700

    Installed grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,25 +6,33 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        copy:{
-            build:{
-                cwd:'app',
-                src:['**'],
-                dest:'build',
-                expand:true
+        copy: {
+            build: {
+                cwd: 'app',
+                src: ['**'],
+                dest: 'build',
+                expand: true
             }
         },
-        clean:{
-            build:{
-                src:'build'
+        clean: {
+            build: {
+                src: 'build'
             }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.registerTask(
-        'build',
-        'Compiles all the assets and copies the files to the build directory.',
-       [ 'clean', 'copy' ]
-    );
+            'build',
+            'Compiles all the assets and copies the files to the build directory.',
+            ['clean', 'copy']
+            );
+    grunt.registerTask('start', function () {
+        grunt.util.spawn(
+                {cmd: 'node'
+                    , args: ['tacmap.js']
+                })
+
+        grunt.task.run('watch:app')
+    })
 };
